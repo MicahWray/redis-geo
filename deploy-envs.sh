615779a9f1934a16d69b151cbbf1afba0c411c63
Micah Wray <micahwray@gmail.com>
2018-07-10 12:31:24 -0500

    Modified deployment

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -6,7 +6,7 @@ export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=us-east-1
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=redisgeo.stickpatrol.com
+export AWS_VIRTUAL_HOST=www.stickpatrol.com
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
